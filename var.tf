039426bfb1045eed2ba141a56f072f04cb09a4e5
manik35 <manikanta@aerospike.com>
2026-02-26 21:04:47 +0530

    terraform

---
+++ var.tf
@@ -0,0 +1,34 @@
+variable "ami_id" {
+    type = string
+  
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+  
+}
+
+variable "sg_ids" {
+    type = list(string)
+  
+}
+
+
+variable "project" {
+    type = string
+  
+}
+
+variable "environment" {
+    type = string
+
+  
+}
+
+variable "tags" {
+    type = map
+    default = {
+    }
+  
+}
